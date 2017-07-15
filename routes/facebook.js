1c0171da88ede67e5c4ed31d5db1d44ad9d168d6
SunSet0418 <cds07016@naver.com>
2017-07-15 19:37:26 +0900

    facebook test

--- routes/facebook.js
+++ routes/facebook.js
@@ -21,18 +21,19 @@ function facebook(app, passport, FacebookStrategy){
         done(null, profile);
     }));
 
-    app.get('/facebook/token', passport.authenticate('facebook-token'), function (req, res) {
-        console.log("user token =========" + req.param('access_token'));
-        if(req.user){
-            var response = {
-                email : req.user.emails[0].value,
-                name : req.user.name.familyName+req.user.name.givenName
-            };
-            res.send(200, response);
-        }
-        else if(!req.user){
-            res.send(401, "Can't find User On Facebook. It May Be Unusable.");
-        }
+    app.get('/facebook/token', passport.authenticate('facebook-token'), (req, res)=>{
+        console.log("user token ========= " + req.param('access_token'));
+        console.log(req.user)
+        // if(req.user){
+        //     var response = {
+        //         email : req.user.emails[0].value,
+        //         name : req.user.name.familyName+req.user.name.givenName
+        //     };
+        //     res.send(200, response);
+        // }
+        // else if(!req.user){
+        //     res.send(401, "Can't find User On Facebook. It May Be Unusable.");
+        // }
     });
 
     app.get('/success', (req, res)=>{
