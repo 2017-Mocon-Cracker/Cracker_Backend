fc9ce343ea4eff040c3c40dd5743685da96ad04a
SunSet0418 <cds07016@naver.com>
2017-07-15 18:29:37 +0900

    facebook change

--- routes/facebook.js
+++ routes/facebook.js
@@ -1,7 +1,6 @@
 module.exports = facebook;
 
-function facebook(app, passport, FacebookStrategy, session) {
-
+function facebook(app, db, passport, FacebookStrategy) {
     app.use(passport.initialize());
     app.use(passport.session());
 
@@ -14,21 +13,35 @@ function facebook(app, passport, FacebookStrategy, session) {
     });
 
     passport.use(new FacebookStrategy({
-        clientID : '132480600677095',
-        clientSecret : '7e7b430487b7580a7941aea2a7e972b4',
-        callbackURL: "/facebook/callback",
+        clientID : '841107499390440',
+        clientSecret : 'e0dcace8cf7df0776b5c0011a1579ece',
+        callbackURL: "/auth/facebook/callback",
         profileFields: ['id', 'email', 'gender', 'link', 'locale', 'name', 'timezone', 'verified'],
     }, (req, accessToken, refreshToken, profile, done)=>{
         console.log(profile)
         done(null, profile)
     }))
 
-    app.get('/facebook/token',
-        passport.authenticate('facebook', { scope : ['email', 'public_profile', 'read_stream', 'publish_actions']})
+    app.get('/auth/facebook/token',
+        passport.authenticate(passport.authenticate('facebook-token'), (req, res)=>{
+            console.log(req.param('access_token'))
+            if(req.user){
+                var response = {
+                    name : req.user.name,
+                    email : req.user.email,
+                    id : req.user.id
+                }
+                res.send(200, response)
+            }
+            else if(!req.user){
+                res.send(401, "Can't find User On Facebook. It May Be Unusable.");
+            }
+
+        })
     )
 
     app.get('/success', (req, res)=>{
-        res.status(200).send('로그인 성공')
+        res.status(200).send("로그인 성공")
     })
 
     app.get('/fail', (req, res)=>{
@@ -36,7 +49,7 @@ function facebook(app, passport, FacebookStrategy, session) {
     })
 
 
-    app.get('/facebook/callback',
+    app.get('/auth/facebook/callback',
         passport.authenticate('facebook', {
             successRedirect: '/success',
             failureRedirect: '/fail'
