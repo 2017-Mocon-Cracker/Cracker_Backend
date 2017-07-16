4d125bcce7b818fd5d8621ce4e4a91e95d468e80
SunSet0418 <cds07016@naver.com>
2017-07-16 23:26:29 +0900

    edit param

--- routes/user.js
+++ routes/user.js
@@ -1,8 +1,9 @@
 module.exports = user
 
-function user(app, db, session){
+function user(app, session, db){
     app.post('/card/add', (req, res)=>{
         var body = req.param('param')
+        console.log(body)
         db.CardInfo.update({
             Email : req.session.email
         },{$set:{CardName:body.CardName, CardIn:true, CardNum:body.CardNum}}, (err)=>{
